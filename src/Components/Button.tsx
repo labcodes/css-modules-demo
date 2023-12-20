fc9875710f41090619a13689e766fe7950ac0cd6
HigorMonteiro <higorvmonteiro@gmail.com>
2023-12-20 16:47:18 -0300

    Initial commit with CSS Modules implementation

---
+++ src/Components/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+type MyButtonProps = {
+  text: string;
+  onClick: () => void;
+};
+
+const MyButton: React.FC<MyButtonProps> = ({ text, onClick }) => {
+  return <button className={styles.button} onClick={onClick}>{text}</button>;
+};
+
+export default MyButton;
